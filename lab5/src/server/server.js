cc58cd3db67eb59f42bfb13119a7bce897702687
Henry Tran <22henrytran@gmail.com>
2016-11-17 12:36:26 -0500

    add express

---
+++ lab5/src/server/server.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var path = require('path');
+var morgan = require('morgan'); // logger
+var bodyParser = require('body-parser');
+var cookieParser = require('cookieParser');
+
+var app = express
