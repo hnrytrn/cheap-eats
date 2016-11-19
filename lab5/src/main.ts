fc8e30c2bec6676cec1181d0bca04c6ab3ad1592
Henry Tran <22henrytran@gmail.com>
2016-11-19 17:44:23 -0500

    add login and register routes

--- lab5/src/main.ts
+++ lab5/src/main.ts
@@ -4,6 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/';
+import { RouterModule, Routes } from '@angular/router';
 
 if (environment.production) {
   enableProdMode();
--- lab5/src/server/server.js
+++ lab5/src/server/server.js
@@ -1,7 +1,23 @@
 var express = require('express');
 var path = require('path');
-var morgan = require('morgan'); // logger
 var bodyParser = require('body-parser');
 var cookieParser = require('cookieParser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
 
-var app = express
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+var app = express();
+app.set('port', (process.env.PORT || 3000));
+
+// Connect to monogo db
+mongoose.connect('mongodb://localhost:27107/lab5');
+
+
+// Models
+
+module.exports = app;
