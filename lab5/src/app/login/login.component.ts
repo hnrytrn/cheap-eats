46d3c3ae901b54d93868a28c18c698bcd7af3084
Henry Tran <22henrytran@gmail.com>
2016-11-20 14:56:42 -0500

    authentication changes

--- lab5/src/app/login/login.component.ts
+++ lab5/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Auth } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: Auth) { }
 
   ngOnInit() {
   }
