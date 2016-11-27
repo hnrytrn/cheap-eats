8b08c31a125d5bf352fbd1a0a2afe22006fc37e4
Henry Tran <htran48@uwo.ca>
2016-11-27 17:58:03 -0500

    food posts page works with db

--- lab5/src/app/add-post/add-post.component.ts
+++ lab5/src/app/add-post/add-post.component.ts
@@ -1,23 +1,59 @@
 import { Component, OnInit } from '@angular/core';
 import { Post } from '../post';
+import { FoodPostService } from '../food-post.service';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-add-post',
   templateUrl: './add-post.component.html',
-  styleUrls: ['./add-post.component.css']
+  styleUrls: ['./add-post.component.css'],
+  providers: [FoodPostService]
 })
 export class AddPostComponent implements OnInit {
+
+  constructor(
+    private foodPostService: FoodPostService
+  ) {}
 
   ngOnInit() {
   }
   
-  model = new Post("", "", "", "", "");
+  private model = new Post("", "", "", "", "");
+  private image; 
 
-  submitted = false
-
-  onSubmit() { this.submitted = false};
+  submitted = false;
   
   postFood() {
-    this.model = new Post("", "", "", "", "");
+    this.submitted = true
+    let postOperation: Observable<Post[]>;
+
+    // Create a new food post
+    postOperation = this.foodPostService.addFoodPost(
+      new Post(this.model.name, this.model.price, this.model.expiryDate, this.model.description, this.image));
+
+    // Subscribe to Observable
+    postOperation.subscribe( 
+      posts => {
+        this.model = new Post("", "", "", "", "");    
+        this.image = null;
+      },
+      err => {
+        console.log(err);
+      }
+    )
+  }
+
+  changeListener($event): void {
+    this.readFile($event.target);
+  }
+
+  readFile(inputeValue: any): void {
+    var file:File = inputeValue.files[0];
+    var reader:FileReader = new FileReader();
+
+    reader.onloadend = (e) => {
+      this.image = reader.result;
+    }
+    reader.readAsDataURL(file);
   }
 }
--- lab5/src/app/food-post.service.ts
+++ lab5/src/app/food-post.service.ts
@@ -1,8 +1,50 @@
 import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Post } from './post';
+import { Observable } from 'rxjs/Rx';
+
+import 'rxjs/add/operator/map' ;
+import 'rxjs/add/operator/catch' ;
 
 @Injectable()
 export class FoodPostService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+  private foodPostsUrl = 'http://localhost:8080/api/foodPost'
 
+  // Get all food posts
+  getFoodPosts(): Observable<Post[]> {
+    return this.http.get(this.foodPostsUrl)
+      .map((res:Response) => res.json())
+      .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+  }
+
+  // Add a new food post
+  addFoodPost(body: Object): Observable<Post[]> {
+    let bodyString = JSON.stringify(body);
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers});
+
+    return this.http.post(this.foodPostsUrl, body, options)
+      .map((res:Response) => res.json())
+      .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+  }
+
+  // Update a food post
+  updateFoodPost(body: Object): Observable<Post[]> {
+    let bodyString = JSON.stringify(body);
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers});
+
+    return this.http.put(`${this.foodPostsUrl}/${body['id']}`, body, options)
+      .map((res:Response) => res.json())
+      .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+  }
+
+  // Delete a food post
+    removeComment (id:string): Observable<Post[]> {
+      return this.http.delete(`${this.foodPostsUrl}/${id}`) 
+      .map((res:Response) => res.json()) 
+      .catch((error:any) => Observable.throw(error.json().error || 'Server error')); 
+    }   
 }
