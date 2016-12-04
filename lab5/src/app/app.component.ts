8ea5aa7090a4d5308df338c17eb42110e5032a66
Henry Tran <htran48@uwo.ca>
2016-12-03 23:37:49 -0500

    allow only retailers to add food posts

--- lab5/src/app/app.component.ts
+++ lab5/src/app/app.component.ts
@@ -8,4 +8,13 @@ import { Auth } from  './auth.service';
 })
 export class AppComponent {
   constructor(private auth: Auth) {}
+
+  // Check if the user is a retailer
+  isRetailer(): boolean {
+    let userMetadata = this.auth.userProfile.user_metadata
+    if (userMetadata != null && userMetadata.retailerName != null) {
+      return true;
+    }
+    return false;
+   }
 }
