8ee5b2b8362af05950fd9a95a4b56a1970786c7d
Henry Tran <htran48@uwo.ca>
2016-12-05 20:00:38 -0500

    fix port

--- server/app.js
+++ server/app.js
@@ -37,8 +37,8 @@ app.use(function(req, res, next) {
 
 
 // start the server
-app.listen(8080, function () {
-    console.log('Server listening on port 8080!');
+app.listen(process.env.PORT ||8080, function () {
+    console.log('Server listening!');
 });
 
 
